71875ccf966718edcd06e43150683e3f70a8b5d6
Ruben <ruben.garcia30102@gmail.com>
2024-10-18 20:11:08 -0500

    completed bookController

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express"
 import cors from "cors"
-import pool from './config/db.js'
-import { getBooks  } from "./controllers/bookController.js";
+import bookRoutes from './routes/bookRoute.js'
 
 const app = express();
 const port = 3000;
@@ -17,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-app.get('/books', getBooks)
+app.use("/api", bookRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
