a58bee60e70ecd5035a2e521efd5c5ccdd8f18ad
Ruben <ruben.garcia30102@gmail.com>
2024-10-28 19:27:25 -0500

    added frontend site

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://library-pied-omega.vercel.app"],
   })
 );
 
